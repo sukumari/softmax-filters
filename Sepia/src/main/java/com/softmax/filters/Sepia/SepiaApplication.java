3f540e754c0d3cb7398b6ddaf34350b2438abb07
Vivekanand Dubey <vdubey@tripwire.com>
2020-11-30 17:18:57 +0530

    Added the sepia filter

---
+++ Sepia/src/main/java/com/softmax/filters/Sepia/SepiaApplication.java
@@ -0,0 +1,13 @@
+package com.softmax.filters.Sepia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepiaApplication.class, args);
+	}
+
+}
